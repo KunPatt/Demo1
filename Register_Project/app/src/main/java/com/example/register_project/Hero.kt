265d4b39fc3e0acd6ff85891032f63674f6bd5c9
KunPatt <77913436+KunPatt@users.noreply.github.com>
2021-04-05 04:00:06 +0700

    Demo321

---
+++ Register_Project/app/src/main/java/com/example/register_project/Hero.kt
@@ -0,0 +1,9 @@
+package com.example.register_project
+
+import android.widget.RatingBar
+
+class Hero (val herod:String, val name:String, val rating: Int) {
+    constructor() : this("","",0) {
+
+    }
+}
